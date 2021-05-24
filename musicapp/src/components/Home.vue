6d8c9b06868acafa89a889b6a1c0a74fbdca694f
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-05-24 14:22:28 +0530

    nav bar and home

---
+++ musicapp/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <form class="form-horizontal">
+        <fieldset>
+          <legend>Welcome to Sarigama</legend>
+          <div class="form-groups">
+            <label for="dynamicSearch" class="col-sm-2 col-form-label">Search Music</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="dynamicSearch" placeholder="Type here..."/>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            Results:
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
